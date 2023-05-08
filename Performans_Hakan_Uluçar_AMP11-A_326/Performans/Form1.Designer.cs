d198f32ca3696d9e79bf1be72c6c7a290cecc127
Haforyx <hakanulucar100@gmail.com>
2023-05-08 21:00:19 +0300

    Performans
    Bu proje bir trene yolcu ekleme silme ve eklenen yolcuların bilgilerini güncelleyebilir bir Tren Yolcu Takip Sistemidir.

---
+++ Performans_Hakan_Uluçar_AMP11-A_326/Performans/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Performans
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 328);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 302);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Koltuk No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yolcu Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yolcu Soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 33);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Gidilecek Yer";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 39);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 39);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(372, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 39);
+            this.textBox3.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Gaziemir",
+            "Adnan Menderes Havalimanı",
+            "Cumaovası ",
+            "Pancar",
+            "Torbalı",
+            "Gürgür ",
+            "Taşkesik",
+            "Arıkbaşı",
+            "Karpuzlu",
+            "Elifli",
+            "Furunlu",
+            "Bayındır",
+            "Yakaköy",
+            "Çatal",
+            "Pınarlı",
+            "Derebaşı",
+            "Doyranlı",
+            "İlkkurşun",
+            "Beytiköy",
+            "Ödemiş"});
+            this.comboBox1.Location = new System.Drawing.Point(536, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 39);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 58);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 58);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(345, 33);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Not : Kalkış Yeri Basmane\'dir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 653);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "HAKAN TURİZM";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Performans_Hakan_Uluçar_AMP11-A_326/Performans/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Performans
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void listele()
+        {
+            SQLiteConnection baglanti = new SQLiteConnection("Data Source=C:\\Users\\hakan\\source\\repos\\Performans\\TrenTakipVeriTabanı.db;version=3");
+            baglanti.Open();
+            SQLiteDataAdapter adaptor = new SQLiteDataAdapter("SELECT * FROM Yolcular", baglanti);
+            DataSet dataSet = new DataSet();
+            adaptor.Fill(dataSet, "Yolcular");
+            dataGridView1.DataSource = dataSet.Tables["Yolcular"];
+            baglanti.Close();
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteConnection baglanti = new SQLiteConnection("Data Source=C:\\Users\\hakan\\source\\repos\\Performans\\TrenTakipVeriTabanı.db;version=3");
+                baglanti.Open();
+                SQLiteCommand komut = new SQLiteCommand();
+                komut.Connection = baglanti;
+                komut.CommandText = "INSERT INTO Yolcular VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + comboBox1.Text + "')";
+                komut.ExecuteNonQuery();
+                listele();
+                baglanti.Close();
+            }
+            catch 
+            {
+              MessageBox.Show("Hata: Bir Sorun Oluştu... \n Hata = Yolcu Eklenemedi...");
+                
+            }
+            finally
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                comboBox1.Text = "";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteConnection baglanti = new SQLiteConnection("Data Source=C:\\Users\\hakan\\source\\repos\\Performans\\TrenTakipVeriTabanı.db;version=3");
+                baglanti.Open();
+                SQLiteCommand komut = new SQLiteCommand();
+                komut.Connection = baglanti;
+                komut.CommandText = "DELETE FROM Yolcular WHERE KoltukNo='" + textBox1.Text + "'";
+                komut.ExecuteNonQuery();
+                listele();
+                baglanti.Close();
+            }
+            catch 
+            {
+                MessageBox.Show("Hata: Bir Sorun Oluştu... \n Hata = Yolcu Silinemedi...");
+            }
+            finally
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection baglanti = new SQLiteConnection("Data Source=C:\\Users\\hakan\\source\\repos\\Performans\\TrenTakipVeriTabanı.db;version=3");
+            baglanti.Open();
+            SQLiteCommand komut = new SQLiteCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "UPDATE Yolcular SET YolcuAdı='" +textBox2.Text+ "', YolcuSoyadı='" +textBox3.Text+ "',GidilecekYer='" +comboBox1.Text + "' WHERE KoltukNo='" + textBox1.Text + "'";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+        }
+    }
+}
